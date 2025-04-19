7af8e669cfc0f1b7de3365823fdfd88aff74a79b
Tygo de Vries <zttdevries@gmail.com>
2025-04-19 23:10:18 +0200

    Updated Shooting

---
+++ Assets/Projectile.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private GameObject ProjectilePrefab;
+
+    public void DrawShot(Shot shot)
+    {
+        Vector3 start = shot.shooter.transform.position;
+        Vector3 end = shot.target.transform.position;
+
+        if(Vector3.Distance(shot.ProjectileEarlyDeathPoint, Vector3.zero) > 0.01f)
+        {
+            end = shot.ProjectileEarlyDeathPoint;
+        }
+
+        Debug.LogWarning(end);
+        ProjectileInstance instance = GameObject.Instantiate(ProjectilePrefab).GetComponent<ProjectileInstance>();
+        
+        instance.SetPoints(start, end);
+    }
+
+    public void Start()
+    {
+        instance = this;
+    }
+    public static Projectile instance;
+}
--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -13,25 +14,41 @@ public class Shoot : MonoBehaviour
         Shot shot = new Shot(GetComponentInParent<Living>(), target.transform.root.gameObject, new Vector3(0, 0.1f, 0));
 
         float chance = shot.GetHitChance();
-        ChanceText.text = "Chance: " + (chance * 100) + "%";
+
+        ChanceText.text = "Chance: " + Mathf.RoundToInt(chance * 100) + "%";
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            if(Random.Range(0f, 1f) <= chance)
+            Projectile.instance.DrawShot(shot);
+
+            if (Random.Range(0f, 1f) <= chance)
             {
                 target.Damage(20);
             }
+            else
+            {
+                Notification.SetText("Miss!", 1f);
+            }
 
-            FindAnyObjectByType<Battle>().UseAction("Attack Shoot");
-            FindAnyObjectByType<Player>().OpenActionGUI();
-            gameObject.SetActive(false);
-            CameraSystem.SetTarget(null);
+            StartCoroutine(DoThing(target.transform));
         }
+    }
+
+    IEnumerator DoThing(Transform target)
+    {
+        CameraSystem.SetTarget(target);
+
+        yield return new WaitForSeconds(1);
+        FindAnyObjectByType<Battle>().UseAction("Attack Shoot");
+        FindAnyObjectByType<Player>().OpenActionGUI();
+        gameObject.SetActive(false);
+
+        CameraSystem.SetTarget(null);
     }
 }
 
 public class Shot
 {
-    private Living shooter;
+    public Living shooter;
     public GameObject target;
     private Vector3 eyeLevel;
 
@@ -44,9 +61,10 @@ public class Shot
 
     public float GetHitChance()
     {
-        return GetHitChanceI(shooter.transform.position + eyeLevel, target, shooter.gameObject, 1.0f, 0);
+        return GetHitChanceI(shooter.transform.position + eyeLevel, target, shooter.gameObject, 1.0f, 0) * LossOverDistance(Vector3.Distance(shooter.transform.position, target.transform.position));
     }
 
+    public Vector3 ProjectileEarlyDeathPoint;
     private float GetHitChanceI(Vector3 origin, GameObject target, GameObject shooter, float baseChance, int age)
     {
         if(age > 100)
@@ -78,6 +96,11 @@ public class Shot
                 ShotChangeEffector effector = hit.collider.transform.root.GetComponent<ShotChangeEffector>();
                 if (effector != null)
                 {
+                    if(effector.PassChance == 0)
+                    {
+                        ProjectileEarlyDeathPoint = hit.point;
+                    }
+
                     baseChance *= effector.PassChance;
                 }
 
@@ -92,4 +115,22 @@ public class Shot
             return 0f;
         }
     }
+
+    private static float LossOverDistance(float distance)
+    {
+        float alwaysHit = 3;
+        float halfHit = 20;
+
+
+        Debug.Log(distance);
+        if (distance < alwaysHit)
+            return 1f;
+
+        if (distance > halfHit)
+            return 0.5f;
+
+        float t = (distance - alwaysHit) / halfHit;
+        return 1f - t * 0.5f;
+    }
+
 }
