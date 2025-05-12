e477815ab9e742fb9ab59b824ba28733a113b3a5
Tygo de Vries <zttdevries@gmail.com>
2025-05-12 13:34:06 +0200

    Added PLAYER

--- Assets/Scripts/WalkPlayer.cs
+++ Assets/Scripts/WalkPlayer.cs
@@ -7,8 +7,10 @@ public class WalkPlayer : MonoBehaviour
     private Rigidbody body;
     private Camera cam;
 
+    private Animator animator;
     private void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         body = GetComponent<Rigidbody>();
         cam = Camera.main;
     }
@@ -16,5 +18,22 @@ public class WalkPlayer : MonoBehaviour
     void Update()
     {
         body.linearVelocity = new Vector3(Input.GetAxis("Horizontal") * PlayerSpeed, body.linearVelocity.y, Input.GetAxis("Vertical") * PlayerSpeed);
+
+        animator.SetBool("IsWalking", body.linearVelocity.magnitude > 0.5f);
+
+        float f = Mathf.Atan2(body.linearVelocity.z, body.linearVelocity.x);
+
+        f *= Mathf.Rad2Deg;
+
+        cur = Mathf.LerpAngle(cur, f, Time.deltaTime * 5);
+
+        Debug.Log(cur);
+
+        float x = Mathf.Cos((cur / 180f) * Mathf.PI);
+        float y = Mathf.Sin((cur / 180f) * Mathf.PI);
+
+        transform.forward = new Vector3(x, 0, y);
     }
+
+    float cur = 0;
 }
