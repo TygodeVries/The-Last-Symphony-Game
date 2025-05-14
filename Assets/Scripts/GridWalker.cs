7b264090b7f88bf06b28ec7029d7919bd7dae456
Tygo de Vries <zttdevries@gmail.com>
2025-05-14 12:45:48 +0200

    Updated Gridwalker to improve smoothness

--- Assets/Scripts/GridWalker.cs
+++ Assets/Scripts/GridWalker.cs
@@ -56,8 +56,8 @@ public class GridWalker : MonoBehaviour
         CameraSystem.SetTarget(transform);
 
         Vector3 startPos = transform.position;
-        float waiting = 0.4f * 0.1f;
-        for (float t = 0; t < 1f; t += 0.1f)
+        float waiting = 0.4f * 0.01f;
+        for (float t = 0; t < 1f; t += 0.01f)
         {
             transform.position = Vector3.Lerp(startPos, tile.transform.position, t);
             yield return new WaitForSeconds(waiting);
