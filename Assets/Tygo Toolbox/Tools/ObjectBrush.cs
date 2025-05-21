ca9ba9f68490f1566fb6e952a3315023ac5a6446
c0zmic <itskelvinay@gmail.com>
2025-05-22 00:15:32 +0200

    added environment stuff exploration 2

--- Assets/Tygo Toolbox/Tools/ObjectBrush.cs
+++ Assets/Tygo Toolbox/Tools/ObjectBrush.cs
@@ -118,7 +118,7 @@ public class ObjectBrushEditor
 
                     float scale = Random.Range(-ObjectBrushWindow.randomScale, ObjectBrushWindow.randomScale);
                     gm.transform.localScale += new Vector3(scale, scale, scale);
-                    gm.transform.localScale /= 120;
+                    gm.transform.localScale /= 500;
                 }
             }
         }
