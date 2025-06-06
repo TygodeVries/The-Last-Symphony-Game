663be0de2a0a3b463ee611ed2b0814550cd0d4f5
Tygo de Vries <zttdevries@gmail.com>
2025-06-06 21:47:32 +0200

    Tooltip text

--- Assets/Scripts/ToolTip.cs
+++ Assets/Scripts/ToolTip.cs
@@ -5,14 +5,21 @@ public class ToolTip : MonoBehaviour
 {
     private static TMP_Text text;
 
+    private static Animator animatior;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animatior = GetComponent<Animator>();
         text = GetComponent<TMP_Text>();
     }
 
     public static void Set(string text)
     {
+        if (ToolTip.text.text != text)
+        {
+            animatior.SetTrigger("Appear");
+        }
         ToolTip.text.text = text;
     }
 
