48161aed976ce663f7d4044363eaffb46e53700f
Tygo de Vries <zttdevries@gmail.com>
2025-06-09 20:11:42 +0200

    Added more text in final battle for clearity

--- Assets/Scripts/Appear.cs
+++ Assets/Scripts/Appear.cs
@@ -11,18 +11,25 @@ public class Appear : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position + offset, Size);
     }
 
-    WalkPlayer walkPlayer;
+    public GameObject tracking;
+
     TMP_Text text;
     public void Start()
     {
+
         text = GetComponent<TMP_Text>();
-        walkPlayer = FindAnyObjectByType<WalkPlayer>();
+
+        if (tracking == null)
+        {
+            tracking = FindAnyObjectByType<WalkPlayer>().gameObject;
+        }
+
         text.color = new Color(0, 0, 0, 0);
     }
 
     private void Update()
     {
-        if (Vector3.Distance(walkPlayer.transform.position, transform.position + offset) < Size)
+        if (Vector3.Distance(tracking.transform.position, transform.position + offset) < Size)
         {
             text.color = Color.Lerp(text.color, text.color = new Color(1, 1, 1, 1), Time.deltaTime * 2);
         }
